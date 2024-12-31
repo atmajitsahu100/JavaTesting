2a4dc71393a64e57757294440fb19402fd8409aa
atmajitsahu100 <atmajitsahoo@gmail.com>
2024-12-31 11:56:48 +0530

    first commit

---
+++ NullPtr.java
@@ -0,0 +1,8 @@
+public class NullPtr{
+    public static void main(String[] args) {
+        String myString = null;
+       
+            int length = myString.length();
+            System.out.println("Length of the string: " + length);
+    }
+}
